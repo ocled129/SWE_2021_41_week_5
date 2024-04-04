b7df1c955cd798d6e54e326e2d0dd0bea0dd6892
Alex Jung <107555838+ocled129@users.noreply.github.com>
2024-04-04 19:37:43 +0900

    Update arrange.sh

--- arrange.sh
+++ arrange.sh
@@ -2,6 +2,6 @@
 
 for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
 do
-    mv files/$i*.txt $i
-    mv files/${i^^}*.txt $i
+    mv ./files/$i*.txt $i
+    mv ./files/${i^^}*.txt $i
 done
